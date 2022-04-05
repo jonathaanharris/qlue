2ed78f4003334684cd2ab7c34c1599a5758e48d5
Jonathan Harris <jonathaanharris@gmail.com>
2022-04-06 00:51:17 +0700

    step4

--- qlue-technical/src/App.js
+++ qlue-technical/src/App.js
@@ -6,10 +6,14 @@ import { useState } from "react"
 function App() {
   let temp = data.data
   let manipulated = []
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
   for (let el of temp) {
     let obj = {}
     obj.id = el.id
-    obj.full_name = el.first_name + el.last_name
+    obj.full_name = `${el.first_name} ${el.last_name}`
     obj.expert_skills = []
     for (let element in el.skills) {
       if (el.skills[element] === "expert") {
@@ -19,9 +23,27 @@ function App() {
     manipulated.push(obj)
   }
   const [user, setUser] = useState(manipulated)
+
+
   return (
     <div className="App">
-
+      <div className='container'>
+        {user.map(el => {
+          return <div key={el.id} className="card">
+            <h3>{el.full_name}</h3>
+            <div>Expert skills:</div>
+            <div>
+              {el.expert_skills.length > 0 ? el.expert_skills.map((element, idx) => {
+                if (idx === el.expert_skills.length - 1) {
+                  return <span> {capitalizeFirstLetter(element)}</span>
+                } else {
+                  return <span> {capitalizeFirstLetter(element)}, </span>
+                }
+              }) : <span>-</span>}
+            </div>
+          </div>
+        })}
+      </div>
     </div>
   );
 }
